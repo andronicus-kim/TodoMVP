2d239b5b5578cd9c9ddd6658f7277c1cf9a363a0
andronicus-kim <androkim.kim7@gmail.com>
2018-05-02 13:12:16 +0300

    sets up TasksDataSource

--- app/src/main/java/io/andronicus/todomvp/data/source/TasksDataSource.java
+++ app/src/main/java/io/andronicus/todomvp/data/source/TasksDataSource.java
@@ -16,5 +16,43 @@
 
 package io.andronicus.todomvp.data.source;
 
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+import io.andronicus.todomvp.data.Task;
+
 public interface TasksDataSource {
+
+    interface LoadTasksCallback{
+        void onTasksLoaded(List<Task> tasks);
+
+        void onDataNotAvailable();
+    }
+    interface GetTaskCallback{
+        void onTaskLoaded(Task task);
+
+        void onDataNotAvailable();
+    }
+    void getTasks(@NonNull LoadTasksCallback callback);
+
+    void getTask(@NonNull String taskId, @NonNull GetTaskCallback callback);
+
+    void saveTask(@NonNull Task task);
+
+    void completeTask(@NonNull Task task);
+
+    void completeTask(@NonNull String taskId);
+
+    void activateTask(@NonNull Task task);
+
+    void activateTask(@NonNull String taskId);
+
+    void clearCompletedTasks();
+
+    void refreshTasks();
+
+    void deleteAllTasks();
+
+    void deleteTask(@NonNull String taskId);
 }
