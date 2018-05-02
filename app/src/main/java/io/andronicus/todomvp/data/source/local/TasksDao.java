5dcc3f855757756c4ad07acf9d99e787a1e21b6d
andronicus-kim <androkim.kim7@gmail.com>
2018-05-02 13:51:08 +0300

    sets up database interactions in TaskDao

--- app/src/main/java/io/andronicus/todomvp/data/source/local/TasksDao.java
+++ app/src/main/java/io/andronicus/todomvp/data/source/local/TasksDao.java
@@ -16,5 +16,54 @@
 
 package io.andronicus.todomvp.data.source.local;
 
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+import io.andronicus.todomvp.data.Task;
+
+@Dao
 public interface TasksDao {
+
+    @Query("SELECT * FROM Tasks")
+    List<Task> getAllTasks();
+
+    @Query("SELECT * FROM Tasks WHERE entryid = :taskId")
+    Task getTaskById(@NonNull String taskId);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertTask(Task task);
+
+    @Update
+    int updateTask(Task task);
+
+    @Query("UPDATE tasks SET completed = :completed WHERE entryid = :taskId")
+    void updateCompleted(@NonNull boolean completed, @NonNull String taskId);
+
+    /**
+     * Delete a task by id.
+     *
+     * @return the number of tasks deleted. This should always be 1.
+     */
+    @Query("DELETE FROM Tasks WHERE entryid = :taskId")
+    int deleteTaskById(String taskId);
+
+    /**
+     * Delete all tasks.
+     */
+    @Query("DELETE FROM Tasks")
+    void deleteTasks();
+
+    /**
+     * Delete all completed tasks from the table.
+     *
+     * @return the number of tasks deleted.
+     */
+    @Query("DELETE FROM Tasks WHERE completed = 1")
+    int deleteCompletedTasks();
 }
